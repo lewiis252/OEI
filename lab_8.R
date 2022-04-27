47d3ec02153a0da92596e708a604e5cb9950507e
lewiis252 <krystianoleniacz@gmail.com>
2022-04-27 09:13:28 +0200

    gitignore

--- lab_8.R
+++ lab_8.R
@@ -1,4 +1,13 @@
 library(FinCal)
+
 # zadanie 1
 przeplywy <- c(-33000,-50000,-5350,17550,22400,32700,35000,11400,21400,21400,56400)
 
+r <- 0.01
+z <- npv(r, przeplywy)
+
+while(z>0){
+  z <- npv(r, przeplywy)
+  r <- r+0.01
+}
+cat('Ostatnie dodatnie r to', r-0.01)
